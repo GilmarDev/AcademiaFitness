fb05b181dc4768f6425a1b7ed152e24f3c399f92
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-29 12:31:18 -0300

    Criado o projeto para academia afim de cadastra modalidades

---
+++ src/main/java/com/academiafitness/cadastro/AcademiaFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.academiafitness.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academiafitness/cadastro/AcademiaFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academiafitness.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
